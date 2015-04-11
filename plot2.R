8e6be27c48459a0f4c663638f0d4504e24623bfb
junquant <junquan.t@gmail.com>
2015-04-11 20:32:21 +0800

    2nd submission

--- plot2.R
+++ plot2.R
@@ -11,10 +11,9 @@ data$Time <- strptime(paste(data$Date,data$Time, sep=" "),"%Y-%m-%d %H:%M:%S")
 # Launch the graphics device. Plot the Graph. Close the graphics device
 png("plot2.png", width = 480, height = 480)
 
-with(data, 
-     plot(data$Time, data$Global_active_power, 
-     xlab = "",
-     ylab = "Global Active Power (kilowatts)",
-     type="l"))
+plot(data$Time, data$Global_active_power, 
+xlab = "",
+ylab = "Global Active Power (kilowatts)",
+type="l")
 
 dev.off()
